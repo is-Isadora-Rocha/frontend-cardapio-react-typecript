61fd4295e51f8ecb58e7f18ce0aa5a0c8cc47957
is-Isadora-Rocha <is.isadora.rocha@gmail..com>
2023-08-18 18:58:05 -0300

    feat: init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    
+      <div className="container">
+        <h1>Cardápio</h1>
+        <div className="card-grid"></div>
+      </div>
+  )
+}
+
+export default App
